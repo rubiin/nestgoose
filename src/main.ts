6902cc665e15950a398b5358e9d2ed7a113a538e
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-04-05 02:40:01 +0545

    feat(pino): add pino

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ConfigService } from '@nestjs/config';
 import setupSwagger from './swagger';
 import { AppUtils } from '@common/helpers/app.utils';
@@ -11,6 +10,7 @@ import {
 	ExpressAdapter,
 	NestExpressApplication,
 } from '@nestjs/platform-express';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(
@@ -41,14 +41,15 @@ async function bootstrap() {
 		}),
 	).setGlobalPrefix('v1');
 
-	// app.useLogger(app.get
-	// (WINSTON_MODULE_NEST_PROVIDER));
+	app.useLogger(app.get(Logger));
 
 	// ==================================================
 	// configureNestSwagger
 	// ==================================================
 
 	setupSwagger(app);
+
+	app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
 	const port = configService.get<number>('app.port', 3000);
 
